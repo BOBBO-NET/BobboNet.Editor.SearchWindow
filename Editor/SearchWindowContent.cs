70b640895df55b6e51fc756fcfc30be2b46b892c
Iseeicy <iseeicyhawkston@gmail.com>
2023-04-25 21:38:19 -0500

    Fixed namespaces

--- Editor/SearchWindowContent.cs
+++ Editor/SearchWindowContent.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 
-namespace BobboNet.Editor.SearchWindow
+namespace BobboNet.Editor
 {
     public abstract class SearchWindowContent : PopupWindowContent
     {
